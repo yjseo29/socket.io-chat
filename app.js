27bd89849b679f2f6c8e4518f5f821171def62c1
yjseo29 <youngjin2960@gmail.com>
2016-03-28 18:17:55 +0900

    기능구현

--- app.js
+++ app.js
@@ -4,19 +4,16 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
 var routes = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 var http = require('http').Server(app);
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -26,31 +23,41 @@ app.set( "ipaddr", "127.0.0.1" );
 app.set( "port", 3000 );
 
 app.use('/', routes);
-app.use('/users', users);
 
 var httpServer = http.listen(app.get('port'), function(){
-    console.log("Express server listening on port " + app.get('port'));
+	console.log("Express server listening on port " + app.get('port'));
 });
 
 var io = require('socket.io').listen(httpServer);
 
+var session = require('express-session')({
+	secret: 'sadfwer',
+	resave: true,
+	saveUninitialized: true
+}),
+sharedsession = require("express-socket.io-session");
+
+app.use(session);
+
 var count = 0;
 var rooms = [];
  
-app.get('/:room',function(req,res){
+/*app.get('/:room',function(req,res){
     console.log('room name is :'+req.params.room);
     res.render('index',{room:req.params.room});
-});
+});*/
+
+io.use(sharedsession(session));
 
 /*** Socket.IO 추가 ***/
-io.sockets.on('connection',function(socket){
-	
+io.on('connection',function(socket){
+
 	socket.on('load_room', function(data){
 		var roomlist = new Array();
 		for(var room in rooms){
 			roomlist.push(rooms[room]);
 		}
-		socket.emit('list_room',{rooms:roomlist});
+		io.sockets.emit('list_room',{rooms:roomlist});
 	});
 
 	/** 닉네임 설정 **/
@@ -60,62 +67,82 @@ io.sockets.on('connection',function(socket){
 
 	/** 접속해제할 경우 **/
 	socket.on('disconnect', function(){
-		//console.log(socket.room);
 		var url = socket.room;
 		if (url != undefined){
-			delete rooms[url].socket_ids[socket.username];
-			io.sockets.in(socket.room).emit('broadcast_msg', {msg:socket.username+"님이 대화방에서 나갔습니다."});
-			io.sockets.in(socket.room).emit('userlist', {users:Object.keys(rooms[url].socket_ids)});
+			delete rooms[url].socket_ids[socket.id];
+			socket.broadcast.to(socket.room).emit('broadcast_msg', {msg:socket.username+"님이 대화방에서 나갔습니다.",type:"notify"});
+			socket.broadcast.to(socket.room).emit('userlist', {users:Object.keys(rooms[url].socket_ids)});
+			if(Object.keys(rooms[url].socket_ids).length == 0){
+				socket.leave(socket.room);
+				delete rooms[url];
+				var roomlist = new Array();
+				for(var room in rooms){
+					roomlist.push(rooms[room]);
+				}
+				io.sockets.emit('list_room',{rooms:roomlist});
+			}
 		}
 		//console.log('user disconnected');
 	});
 
 	/** 채팅방 만들기 **/
 	socket.on('create_room', function(data){
+
 		var url = data.url;
-		var nickname =  socket.username;
-
-		socket.room = url; //소켓 방 설정
-		socket.join(url); //소켓방에 접속
-
-		/** 채팅방 생성 **/
 		if (rooms[url] == undefined) {
 			rooms[url] = new Object();
 			rooms[url].title = data.title;
 			rooms[url].url = url;
 			rooms[url].socket_ids = new Object();
 		}
-
-		rooms[url].socket_ids[nickname] = socket.id;
 		
 		var roomlist = new Array();
 		for(var room in rooms){
 			roomlist.push(rooms[room]);
 		}
-		socket.broadcast.emit('list_room',{rooms:roomlist});
-
-		io.sockets.in(socket.room).emit('broadcast_msg', {msg:nickname+"님이 대화방에 입장하였습니다"});
-		io.sockets.in(socket.room).emit('userlist', {users:Object.keys(rooms[url].socket_ids)});
+		io.sockets.emit('list_room',{rooms:roomlist});
 	});
 
 	/** 채팅방 접속 **/
 	socket.on('join_room', function(data){
-		var url = data.url;
-		var nickname = data.nickname;
+		//다른 채팅방에서 옮겨왔을경우 처리
+		if(socket.room != undefined){
+			delete rooms[socket.room].socket_ids[socket.id];
+			socket.broadcast.to(socket.room).emit('broadcast_msg', {msg:socket.username+"님이 대화방에서 나갔습니다.",type:"notify"});
+			socket.broadcast.to(socket.room).emit('userlist', {users:Object.keys(rooms[socket.room].socket_ids)});
+			if(Object.keys(rooms[socket.room].socket_ids).length == 0){
+				socket.leave(socket.room);
+				delete rooms[socket.room];
+				var roomlist = new Array();
+				for(var room in rooms){
+					roomlist.push(rooms[room]);
+				}
+				io.sockets.emit('list_room',{rooms:roomlist});
+			}
+		}
 
-		socket.username = nickname; //소켓 네임 설정
-		socket.room = url; //소켓 방 설정
-		socket.join(url); //소켓방에 접속
+		if (rooms[data.url] != undefined) {
+			var url = data.url;
+			var nickname = socket.username;
 
-		rooms[url].socket_ids[nickname] = socket.id;
-		io.sockets.in(socket.room).emit('broadcast_msg', {msg:nickname+"님이 대화방에 입장하였습니다"});
-		io.sockets.in(socket.room).emit('userlist', {users:Object.keys(rooms[url].socket_ids)});
+			socket.room = url; //소켓 방 설정
+			socket.join(url); //소켓방에 접속
+
+			rooms[url].socket_ids[socket.id] = nickname;
+			console.log(rooms);
+			io.sockets.in(url).emit('broadcast_msg', {msg:nickname+"님이 대화방에 입장하였습니다",type:"notify"});
+			io.sockets.in(url).emit('userlist', {users:rooms[url].socket_ids[Object.keys(rooms[url].socket_ids)]});
+		}
 	});
  
 	/** 메시지 전송 처리 **/
 	socket.on('send_msg',function(data){
-		data.msg = socket.username + ' : ' + data.msg;
-		io.sockets.in(socket.room).emit('broadcast_msg', {msg:data.msg,username:socket.username});
+		io.sockets.in(socket.room).emit('broadcast_msg', {msg:data.msg,username:socket.username,type:"message"});
+	});
+
+	/** 이미지 업로드 처리 **/
+	socket.on('image_upload',function(data){
+		io.sockets.in(socket.room).emit('broadcast_msg', {msg:data.baseurl,username:socket.username,type:"image"});
 	});
 }); 
 
